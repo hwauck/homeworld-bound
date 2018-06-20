4cbe8fee1200f1242b4ca2e28879b047d31b16f0
Helen Wauck <hwauck@gmail.com>
2018-06-20 17:16:49 +0200

    started working on the b2 scene portion of tutorial

--- Assets/_Script/Construction Mode/Tutorial2.cs
+++ Assets/_Script/Construction Mode/Tutorial2.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Tutorial2 : MonoBehaviour {
-	public GameObject finishedImage;
+    public bool disableTutorial;
+
+    public GameObject finishedImage;
 	public Button[] partButtons;
 
 	public Highlighter highlighter;
@@ -16,7 +18,22 @@ public class Tutorial2 : MonoBehaviour {
 	private GameObject selectedObj;
 	private GameObject conversationSystem;
 
-	private bool done;
+    public CanvasGroup bottomPanel;
+    public CanvasGroup timeRemainingPanel;
+    public CanvasGroup rotationsRemainingPanel;
+    public Text timeRemaining;
+    public Text rotationsRemaining;
+    private bool enabledControls;
+    private CameraControls cameraControls;
+    public GameObject rotationGizmo;
+    private RotationGizmo rotationScript;
+    public Button fuseButton;
+    public Button controlsButton;
+    public AudioSource audioSource;
+    public AudioClip lowPowerSound;
+    public Text lowPowerText;
+
+    private bool done;
 
 	void Awake() {
 
@@ -25,34 +42,79 @@ public class Tutorial2 : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		selectPart = eventSystem.GetComponent<SelectPart>();
+        rotationScript = rotationGizmo.GetComponent<RotationGizmo>();
+        selectPart = eventSystem.GetComponent<SelectPart>();
 		fuseEvent = eventSystem.GetComponent<FuseEvent>();
 		conversationSystem = GameObject.Find("ConversationSystem");
+        enabledControls = false;
 
-	}
+        if (disableTutorial)
+        {
+            enabledControls = true;
+            bottomPanel.blocksRaycasts = true;
+            cameraControls.tutorialMode = false;
+            rotationScript.tutorialMode = false;
+            selectPart.tutorialMode = false;
+            ConversationTrigger.AddToken("finishedEnablingControls2");
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-
+        // low power warning flashes on screen
+        if(!ConversationTrigger.GetToken("finishedConst_21"))
+        {
+            StartCoroutine(flashLowPowerAndAddToken());
+        }
+        // show timer at top
+        else if(!ConversationTrigger.GetToken("finishedConst_22") && ConversationTrigger.GetToken("finishedConst_21"))
+        {
+            //set canvas group alpha to 1 for both time and rotations
+            //set blocks raycasts = true from both time and rotations
+            timeRemainingPanel.alpha = 1;
+            rotationsRemainingPanel.alpha = 1;
+            timeRemainingPanel.blocksRaycasts = true;
+            rotationsRemainingPanel.blocksRaycasts = true;
+        }
 	}
-		
 
-	private void highlightSelectedObj(float sec) { // generalizes to any selectedObj
-		GameObject selectedObj = selectPart.getActivePart();
-		if(selectedObj.name.Equals("tutorial2_bigboxPrefab(Clone)")) {
-			highlighter.HighlightTimed(GameObject.Find("bigbox_close"), sec); 
-			highlighter.HighlightTimed(GameObject.Find("bigbox_far"), sec); 
-		} else if(selectedObj.name.Equals("tutorial2_smallbox_bluePrefab(Clone)")) {
-			highlighter.HighlightTimed(GameObject.Find("smallbox_blue"), sec); 
-		} else if(selectedObj.name.Equals("tutorial2_smallbox_yellowPrefab(Clone)")) {
-			highlighter.HighlightTimed(GameObject.Find("smallbox_yellow"), sec); 
-		} else {
-			highlighter.HighlightTimed(GameObject.Find("tallbox"), sec); 
-		}
-	}
+    private IEnumerator flashLowPowerAndAddToken()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            audioSource.PlayOneShot(lowPowerSound);
+            lowPowerText.enabled = true;
+            yield return new WaitForSeconds(1f);
+            lowPowerText.enabled = false;
+            yield return new WaitForSeconds(1f);
+        }
+        ConversationTrigger.AddToken("finishedLowPowerWarning");
+    }
 
-	private void highlightPartButtons(float sec) {
+    private void flashControlsAndAddToken(string token)
+    {
+        highlightGizmo(1f);
+        for (int i = 0; i < partButtons.Length; i++)
+        {
+            highlighter.HighlightTimed(partButtons[i].gameObject, 1f);
+
+        }
+        highlighter.HighlightTimed(fuseButton.gameObject, 1f);
+        highlighter.HighlightTimed(controlsButton.gameObject, 1f);
+        ConversationTrigger.AddToken(token);
+    }
+
+    private void highlightGizmo(float seconds)
+    {
+        // maybe should highlight only the sliders instead?
+        foreach (Transform child in rotationGizmo.transform)
+        {
+            highlighter.HighlightTimed(child.gameObject, seconds);
+        }
+    }
+
+    private void highlightPartButtons(float sec) {
 		foreach (Button b in partButtons) {
 			highlighter.HighlightTimed(b.gameObject, sec);
 		}
