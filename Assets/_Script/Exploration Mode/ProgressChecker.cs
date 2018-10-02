90485fc60e1f6d5c7d6d17091336fb51f26af9b7
sebtsa <sebastian.saraceno@gmx.ch>
2018-10-02 18:46:03 +0200

    Starting position fixed Signed-off-by: sebtsa <sebastian.saraceno@gmx.ch>

--- Assets/_Script/Exploration Mode/ProgressChecker.cs
+++ Assets/_Script/Exploration Mode/ProgressChecker.cs
@@ -3,22 +3,31 @@ using System.Collections;
 
 public class ProgressChecker : MonoBehaviour
 {
-	// When loading a save, this script checks to see if the player should be in a different level.
-	// If they should be, it relocates them.
+    // When loading a save, this script checks to see if the player should be in a different level.
+    // If they should be, it relocates them.
 
-	void Start ()
-	{
-		// Later levels should be higher in this list.
-		if (ConversationTrigger.GetToken("reachedLevel_CityVault"))
-		{
-			LoadUtils.LoadNewExplorationLevel("CityVault", new Vector3(0,5,0));
-			return;
-		}
+    void Start()
+    {
+        
 
-		if (ConversationTrigger.GetToken("reachedLevel_RuinedCity"))
-		{
-			LoadUtils.LoadNewExplorationLevel("RuinedCity", new Vector3(0,5,0));
-			return;
-		}
+
+        // Later levels should be higher in this list.
+        if (ConversationTrigger.GetToken("reachedLevel_CityVault"))
+        {
+            LoadUtils.LoadNewExplorationLevel("CityVault", new Vector3(0, 5, 0));
+            return;
+        }
+
+        if (ConversationTrigger.GetToken("reachedLevel_RuinedCity"))
+        {
+            LoadUtils.LoadNewExplorationLevel("RuinedCity", new Vector3(0, 5, 0));
+            return;
+        }
+        else
+        {
+            transform.position = new Vector3(-160, -32, 60);
+            Debug.Log("transformierung");
+        }
+       
 	}
 }
